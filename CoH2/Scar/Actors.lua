da2f12d5460b55108c3a82add68fda9e07bb5eaf
CoDiEx <jako4554@yahoo.dk>
2015-09-22 00:23:59 +0200

    Create Actors.lua
    I don't claim any of this! This is only here so I don't have to open the archives each time I want a new portrait

---
+++ CoH2/Scar/Actors.lua
@@ -0,0 +1,70 @@
+ACTOR = {
+
+	None					= "",
+
+	-- Generic Russians
+	Russian_Commissar		= "Icons_portraits_dialogue_soviet_commissar_s_portrait",
+	Russian_Junior_Officer	= "Icons_portraits_dialogue_soviet_poznan_officer_01_w_portrait", -- Poznan_01 looks junior. No unique art for "junior officer".
+	Russian_Senior_Officer	= "Icons_portraits_dialogue_soviet_senior_officer_s_portrait",
+	Russian_Radio_Command	= "Icons_portraits_dialogue_soviet_command_radio_s_portrait",
+	
+	Russian_Soldier_01		= "Icons_portraits_dialogue_soviet_soldier_01_s_portrait",
+	Russian_Soldier_02		= "Icons_portraits_dialogue_soviet_soldier_02_s_portrait",
+	Russian_Soldier_03		= "Icons_portraits_dialogue_soviet_soldier_03_s_portrait",
+	Russian_Soldier_04		= "Icons_portraits_dialogue_soviet_soldier_04_s_portrait",
+	Russian_Soldier_05		= "Icons_portraits_dialogue_soviet_soldier_05_s_portrait",
+	Russian_Soldier_06		= "Icons_portraits_dialogue_soviet_soldier_06_s_portrait",
+	Russian_Soldier_07		= "Icons_portraits_dialogue_soviet_soldier_07_s_portrait",
+	
+	Russian_Sniper			= "Icons_portraits_dialogue_soviet_sniper_s_portrait",
+	
+	Russian_Engineer		= "Icons_portraits_dialogue_soviet_engineer_s_portrait",
+	
+	Russian_Tank_Commander	= "Icons_portraits_dialogue_soviet_tank_commander_s_portrait",
+	Russian_Tank_Gunner		= "Icons_portraits_dialogue_soviet_tank_gunner_s_portrait",
+	Russian_Tank_Officer	= "Icons_portraits_dialogue_soviet_tank_officer_s_portrait",
+	
+	Civilian				= "Icons_portraits_dialogue_civilian_s_portrait",
+	Civilian_Female			= "Icons_portraits_dialogue_civilian_female_s_portrait",
+	
+	Partisans				= "Icons_portraits_dialogue_partisan_male_s_portrait",
+	Partisans_Female 		= "Icons_portraits_dialogue_partisan_female_s_portrait",
+	
+	-- CoH2 Campaign
+	Ania					= "Icons_portraits_dialogue_ania_s_portrait",
+	Churkin					= "Icons_portraits_dialogue_churkin_s_portrait",
+	Isakovich				= "Icons_portraits_dialogue_isakovich_s_portrait",
+	Polivanov				= "Icons_portraits_dialogue_major_polivanov_s_portrait",
+	Pozharsky				= "Icons_portraits_dialogue_pozharski_s_portrait",
+	Yuri					= "Icons_portraits_dialogue_yuri_s_portrait",
+	
+	Poznan_Officer_01		= "Icons_portraits_dialogue_soviet_poznan_officer_01_w_portrait",
+	Poznan_Officer_02		= "Icons_portraits_dialogue_soviet_poznan_officer_02_w_portrait",
+	
+	-- Generic Germans
+	German_Officer			= "Icons_portraits_dialogue_german_officer_s_portrait",
+	German_Ostruppen		= "Icons_portraits_dialogue_german_ostruppen_s_portrait",
+	German_Grenadier		= "Icons_portraits_dialogue_german_grenadier_s_portrait",
+	German_Panzer_Grenadier	= "Icons_portraits_dialogue_german_panzer_grenadier_s_portrait",
+	German_Artillery		= "Icons_portraits_dialogue_german_artillery_officer_s_portrait",
+	
+	-- CoH2 XP1
+	Derby					= {nameID = 11077130, icon = "Icons_bob_companies_dialog_support"},					-- Dog Company – Support
+	Vastano					= {nameID = 11077122, icon = "Icons_bob_companies_dialog_airborne"},				-- Able Company - Airborne
+	Edwards					= {nameID = 11077126, icon = "Icons_bob_companies_dialog_infantry"},				-- Baker Company - Mechanized
+	Durante					= {nameID = 11081058, icon = "Icons_bob_companies_dialog_ranger"},					-- Fox Company - Ranger
+	Jackson					= {nameID = 11077132, icon = "Icons_portraits_dialogue_aef_captain_w_portrait"},	-- the commander in the intro mission, temp since we have no jackson portrait
+	
+	Ouren_112th 			= {nameID = 11080987, icon = ""},	
+	Houffalize_1st 			= {nameID = 11080988, icon = ""},	
+	
+	-- Generic Americans
+	American_Riflemen_01	= "Icons_portraits_dialogue_aef_rifleman_w_portrait",
+	American_Engineer_01	= "Icons_portraits_dialogue_aef_assault_engineer_w_portrait",
+	American_Paratrooper_01	= "Icons_portraits_dialogue_aef_paratrooper_w_portrait", -- temp
+	
+	American_Lieutenant_01	= "Icons_portraits_dialogue_aef_lieutenant_w_portrait",
+	American_Captain_01		= "Icons_portraits_dialogue_aef_captain_w_portrait",
+	American_Major_01		= "Icons_portraits_dialogue_aef_major_w_portrait",
+	
+}
